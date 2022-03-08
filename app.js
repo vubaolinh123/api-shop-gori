2d19b7bd6371996229ae7c45de7b0500a8404ebf
Link <73287503+vubaolinh123@users.noreply.github.com>
2022-03-08 15:54:26 +0700

    Viết API ở và call lại ở index.html

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const express = require('express')
+const cors = require('cors');
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("<h1>Home Page</h1>")
@@ -13,6 +15,16 @@ app.get("/api/products", (req, res) => {
     ];
     res.json(data)
 })
+
+app.get("/api/users", (req, res) => {
+    const data = [
+        { id: 1, name: "Name A", email: "abc@gmail.com", address: "VietNam" },
+        { id: 2, name: "Name B", email: "abcx@gmail.com", address: "HaNoi" },
+        { id: 3, name: "Name C", email: "abcbx@gmail.com", address: "HaNoi" }
+    ];
+    res.send(data)
+
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
