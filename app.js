98948ccf30a59ba544ae54ea831d7aa9a334d0a0
Link <73287503+vubaolinh123@users.noreply.github.com>
2022-03-08 15:01:48 +0700

    Update express

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    const url = req.url;
+    if (url === "/api/products") {
+        const data = [
+            {
+                id: 1, name: "Product A"
+            },
+            {
+                id: 2, name: "Product B"
+            }
+        ];
+        res.end(JSON.stringify(data))
+    } else if (url === "/api/posts") {
+        console.log("API POST");
+    } else {
+        res.setHeader("Content-Type", "text/html");
+        res.write("<html><body><h1>Home Page</h1></body></html>");
+        res.end()
+    }
 })
 
 const PORT = 3001;
